86996a73dec7844e2a03a5e68ad1b89ae350e322
赵昀 <632896862@qq.com>
2021-11-09 15:15:30 +0800

    perf(add eslint): add eslint

--- src/tileCorrection.js
+++ src/tileCorrection.js
@@ -1,14 +1,7 @@
-/* eslint-disable */
-import {
-  toPoint
-} from 'leaflet/src/geometry/Point'
-import {
-  toLatLng
-} from 'leaflet/src/geo/LatLng'
-import {
-  Bounds
-} from 'leaflet/src/geometry/Bounds'
-import {Point} from 'leaflet/src/geometry/Point'
+import { toPoint, Point } from 'leaflet/src/geometry/Point'
+import { toLatLng } from 'leaflet/src/geo/LatLng'
+import { Bounds } from 'leaflet/src/geometry/Bounds'
+
 import { toLatLngBounds as latLngBounds } from 'leaflet/src/geo/LatLngBounds'
 import * as DomUtil from 'leaflet/src/dom/DomUtil'
 import * as Browser from 'leaflet/src/core/Browser'
@@ -16,31 +9,43 @@ import * as Util from 'leaflet/src/core/Util'
 L.Map.include({
   project: function (latlng, zoom, crs) {
     zoom = zoom === undefined ? this._zoom : zoom
-    return crs ? this.options.customCRS[crs].crs.latLngToPoint(toLatLng(latlng), zoom) : this.options.crs.latLngToPoint(toLatLng(latlng), zoom)
+    return crs
+      ? this.options.customCRS[crs].crs.latLngToPoint(toLatLng(latlng), zoom)
+      : this.options.crs.latLngToPoint(toLatLng(latlng), zoom)
   },
   unproject: function (point, zoom, crs) {
     zoom = zoom === undefined ? this._zoom : zoom
-    return crs ? this.options.customCRS[crs].crs.pointToLatLng(toPoint(point), zoom) : this.options.crs.pointToLatLng(toPoint(point), zoom)
+    return crs
+      ? this.options.customCRS[crs].crs.pointToLatLng(toPoint(point), zoom)
+      : this.options.crs.pointToLatLng(toPoint(point), zoom)
   },
   getZoomScale: function (toZoom, fromZoom, crsName) {
-    var crs = this.options.crs
+    let crs = this.options.crs
     fromZoom = fromZoom === undefined ? this._zoom : fromZoom
     if (crsName) {
       const startZoom = this.options.customCRS[crsName].startZoom
       console.log(startZoom)
       toZoom -= this.options.customCRS[crsName].startZoom
       crs = this.options.customCRS[crsName].crs
-      fromZoom = fromZoom || this._zoom - this.options.customCRS[crsName].startZoom
+      fromZoom =
+        fromZoom || this._zoom - this.options.customCRS[crsName].startZoom
     }
     return crs.scale(toZoom) / crs.scale(fromZoom)
   },
   _getNewPixelOrigin: function (center, zoom, crs) {
-    var viewHalf = this.getSize()._divideBy(2)
-    return this.project(center, zoom, crs)._subtract(viewHalf)._add(this._getMapPanePos())._round()
+    const viewHalf = this.getSize()._divideBy(2)
+    return this.project(center, zoom, crs)
+      ._subtract(viewHalf)
+      ._add(this._getMapPanePos())
+      ._round()
   },
   _getNewCenter: function (point, zoom, crs) {
-    var viewHalf = this.getSize()._divideBy(2)
-    return this.unproject(toPoint(point)._subtract(this._getMapPanePos())._add(viewHalf), zoom, crs)
+    const viewHalf = this.getSize()._divideBy(2)
+    return this.unproject(
+      toPoint(point)._subtract(this._getMapPanePos())._add(viewHalf),
+      zoom,
+      crs
+    )
   },
   getPixelOrigin: function (crs) {
     this._checkIfLoaded()
@@ -50,23 +55,24 @@ L.Map.include({
     if (zoom === undefined) {
       zoom = this._zoom
     }
-    var zoomChanged = this._zoom !== zoom
+    const zoomChanged = this._zoom !== zoom
 
     this._zoom = zoom
     this._lastCenter = center
     this._pixelOrigin = this._getNewPixelOrigin(center)
 
-    var crss = Object.keys(this.options.customCRS)
-    crss.forEach(crs => {
+    const crss = Object.keys(this.options.customCRS)
+    crss.forEach((crs) => {
       if (zoom >= this.options.customCRS[crs].startZoom) {
-        var tileZoom = zoom - this.options.customCRS[crs].startZoom
+        const tileZoom = zoom - this.options.customCRS[crs].startZoom
         this[crs] = this._getNewPixelOrigin(center, tileZoom, crs)
       }
     })
     // @event zoom: Event
     // Fired repeatedly during any change in zoom level, including zoom
     // and fly animations.
-    if (zoomChanged || (data && data.pinch)) { // Always fire 'zoom' if pinching because #3530
+    if (zoomChanged || (data && data.pinch)) {
+      // Always fire 'zoom' if pinching because #3530
       this.fire('zoom', data)
     }
 
@@ -78,8 +84,11 @@ L.Map.include({
 })
 L.GridLayer.include({
   _setView: function (center, zoom, noPrune, noUpdate) {
-    var tileZoom = Math.round(zoom)
-    if ((this.options.maxZoom !== undefined && tileZoom > this.options.maxZoom) || (this.options.minZoom !== undefined && tileZoom < this.options.minZoom)) {
+    let tileZoom = Math.round(zoom)
+    if (
+      (this.options.maxZoom !== undefined && tileZoom > this.options.maxZoom) ||
+      (this.options.minZoom !== undefined && tileZoom < this.options.minZoom)
+    ) {
       tileZoom = undefined
     } else {
       tileZoom = this._clampZoom(tileZoom)
@@ -92,7 +101,8 @@ L.GridLayer.include({
       }
     }
 
-    var tileZoomChanged = this.options.updateWhenZooming && (tileZoom !== this._tileZoom)
+    const tileZoomChanged =
+      this.options.updateWhenZooming && tileZoom !== this._tileZoom
 
     if (!noUpdate || tileZoomChanged) {
       this._tileZoom = tileZoom
@@ -120,30 +130,46 @@ L.GridLayer.include({
     this._setZoomTransforms(center, zoom)
   },
   _update: function (center) {
-    var map = this._map
-    if (!map) { return }
-    var zoom = this._clampZoom(map.getZoom())
+    const map = this._map
+    if (!map) {
+      return
+    }
 
-    if (center === undefined) { center = map.getCenter() }
-    if (this._tileZoom === undefined) { return }	// if out of minzoom/maxzoom
+    if (center === undefined) {
+      center = map.getCenter()
+    }
+    if (this._tileZoom === undefined) {
+      return
+    } // if out of minzoom/maxzoom
 
-    var pixelBounds = this._getTiledPixelBounds(center),
-		    tileRange = this._pxBoundsToTileRange(pixelBounds),
-		    tileCenter = tileRange.getCenter(),
-		    queue = [],
-		    margin = this.options.keepBuffer,
-		    noPruneRange = new Bounds(tileRange.getBottomLeft().subtract([margin, -margin]),
-		                              tileRange.getTopRight().add([margin, -margin]))
+    const pixelBounds = this._getTiledPixelBounds(center)
+    const tileRange = this._pxBoundsToTileRange(pixelBounds)
+    const tileCenter = tileRange.getCenter()
+    const queue = []
+    const margin = this.options.keepBuffer
+    const noPruneRange = new Bounds(
+      tileRange.getBottomLeft().subtract([margin, -margin]),
+      tileRange.getTopRight().add([margin, -margin])
+    )
 
     // Sanity check: panic if the tile range contains Infinity somewhere.
-    if (!(isFinite(tileRange.min.x) &&
-		      isFinite(tileRange.min.y) &&
-		      isFinite(tileRange.max.x) &&
-		      isFinite(tileRange.max.y))) { throw new Error('Attempted to load an infinite number of tiles') }
+    if (
+      !(
+        isFinite(tileRange.min.x) &&
+        isFinite(tileRange.min.y) &&
+        isFinite(tileRange.max.x) &&
+        isFinite(tileRange.max.y)
+      )
+    ) {
+      throw new Error('Attempted to load an infinite number of tiles')
+    }
 
-    for (var key in this._tiles) {
-      var c = this._tiles[key].coords
-      if (c.z !== this._tileZoom || !noPruneRange.contains(new Point(c.x, c.y))) {
+    for (const key in this._tiles) {
+      const c = this._tiles[key].coords
+      if (
+        c.z !== this._tileZoom ||
+        !noPruneRange.contains(new Point(c.x, c.y))
+      ) {
         this._tiles[key].current = false
       }
     }
@@ -153,14 +179,16 @@ L.GridLayer.include({
     // if (Math.abs(zoom - this._tileZoom) > 1) { this._setView(center, zoom); return }
 
     // create a queue of coordinates to load tiles from
-    for (var j = tileRange.min.y; j <= tileRange.max.y; j++) {
-      for (var i = tileRange.min.x; i <= tileRange.max.x; i++) {
-        var coords = new Point(i, j)
+    for (let j = tileRange.min.y; j <= tileRange.max.y; j++) {
+      for (let i = tileRange.min.x; i <= tileRange.max.x; i++) {
+        const coords = new Point(i, j)
         coords.z = this._tileZoom
 
-        if (!this._isValidTile(coords)) { continue }
+        if (!this._isValidTile(coords)) {
+          continue
+        }
 
-        var tile = this._tiles[this._tileCoordsToKey(coords)]
+        const tile = this._tiles[this._tileCoordsToKey(coords)]
         if (tile) {
           tile.current = true
         } else {
@@ -184,9 +212,9 @@ L.GridLayer.include({
       }
 
       // create DOM fragment to append tiles in one batch
-      var fragment = document.createDocumentFragment()
+      const fragment = document.createDocumentFragment()
 
-      for (i = 0; i < queue.length; i++) {
+      for (let i = 0; i < queue.length; i++) {
         this._addTile(queue[i], fragment)
       }
 
@@ -194,11 +222,11 @@ L.GridLayer.include({
     }
   },
   _updateLevels: function () {
-    var zoom = this._tileZoom,
-      maxZoom = this.options.maxZoom
+    const zoom = this._tileZoom
+    const maxZoom = this.options.maxZoom
 
     function remove (el) {
-      var parent = el.parentNode
+      const parent = el.parentNode
       if (parent) {
         parent.removeChild(el)
       }
@@ -207,7 +235,7 @@ L.GridLayer.include({
       return undefined
     }
 
-    for (var z in this._levels) {
+    for (let z in this._levels) {
       z = Number(z)
       if (this._levels[z].el.children.length || z === zoom) {
         this._levels[z].el.style.zIndex = maxZoom - Math.abs(zoom - z)
@@ -220,18 +248,31 @@ L.GridLayer.include({
       }
     }
 
-    var level = this._levels[zoom],
-      map = this._map
+    let level = this._levels[zoom]
+    const map = this._map
 
     if (!level) {
       level = this._levels[zoom] = {}
-      level.el = DomUtil.create('div', 'leaflet-tile-container leaflet-zoom-animated', this._container)
+      level.el = DomUtil.create(
+        'div',
+        'leaflet-tile-container leaflet-zoom-animated',
+        this._container
+      )
       level.el.style.zIndex = maxZoom
-      level.origin = map.project(map.unproject(map.getPixelOrigin()), map.getZoom()).round()
+      level.origin = map
+        .project(map.unproject(map.getPixelOrigin()), map.getZoom())
+        .round()
       if (this.options.crs) {
-        var crs = this.options.crs
-        level[crs] = map.project(map.unproject(map.getPixelOrigin(crs), zoom, crs), zoom, crs).round()
-        console.log('getPixelOrigin:' + map.getPixelOrigin() + crs + map.getPixelOrigin(crs))
+        const crs = this.options.crs
+        level[crs] = map
+          .project(map.unproject(map.getPixelOrigin(crs), zoom, crs), zoom, crs)
+          .round()
+        console.log(
+          'getPixelOrigin:' +
+            map.getPixelOrigin() +
+            crs +
+            map.getPixelOrigin(crs)
+        )
         console.log('origin:' + level.origin + crs + level[crs])
       }
       level.zoom = zoom
@@ -249,28 +290,40 @@ L.GridLayer.include({
     return level
   },
   _getTiledPixelBounds: function (center) {
-    var map = this._map,
-      mapZoom = map._animatingZoom ? Math.max(map._animateToZoom, map.getZoom()) : map.getZoom(),
-      scale = map.getZoomScale(mapZoom, this._tileZoom, this.options.crs),
-      pixelCenter = map.project(center, this._tileZoom, this.options.crs).floor(),
-      halfSize = map.getSize().divideBy(scale * 2)
+    const map = this._map
+    const mapZoom = map._animatingZoom
+      ? Math.max(map._animateToZoom, map.getZoom())
+      : map.getZoom()
+    const scale = map.getZoomScale(mapZoom, this._tileZoom, this.options.crs)
+    const pixelCenter = map
+      .project(center, this._tileZoom, this.options.crs)
+      .floor()
+    const halfSize = map.getSize().divideBy(scale * 2)
 
-    return new Bounds(pixelCenter.subtract(halfSize), pixelCenter.add(halfSize))
+    return new Bounds(
+      pixelCenter.subtract(halfSize),
+      pixelCenter.add(halfSize)
+    )
   },
   _getTilePos: function (coords) {
-    let origin = this.options.crs ? this._level[this.options.crs] : this._level.origin
+    const origin = this.options.crs
+      ? this._level[this.options.crs]
+      : this._level.origin
     return coords.scaleBy(this.getTileSize()).subtract(origin)
   },
   _isValidTile: function (coords) {
-    var crs = this._map.options.crs
+    let crs = this._map.options.crs
     if (this.options.crs) {
       crs = this._map.options.customCRS[this.options.crs].crs
     }
     if (!crs.infinite) {
       // don't load tile if it's out of bounds and not wrapped
-      var bounds = this._globalTileRange
-      if ((!crs.wrapLng && (coords.x < bounds.min.x || coords.x > bounds.max.x)) ||
-        (!crs.wrapLat && (coords.y < bounds.min.y || coords.y > bounds.max.y))) {
+      const bounds = this._globalTileRange
+      if (
+        (!crs.wrapLng &&
+          (coords.x < bounds.min.x || coords.x > bounds.max.x)) ||
+        (!crs.wrapLat && (coords.y < bounds.min.y || coords.y > bounds.max.y))
+      ) {
         return false
       }
     }
@@ -280,16 +333,20 @@ L.GridLayer.include({
     }
 
     // don't load tile if it doesn't intersect the bounds in options
-    var tileBounds = this._tileCoordsToBounds(coords)
+    const tileBounds = this._tileCoordsToBounds(coords)
     return latLngBounds(this.options.bounds).overlaps(tileBounds)
   },
   _setZoomTransform: function (level, center, zoom) {
     // zoom = this.options.minZoom ? zoom - this.options.minZoom : zoom
-    var scale = this._map.getZoomScale(zoom, level.zoom, this.options.crs),
-      origin = this.options.crs ? level[this.options.crs] : level.origin,
-      zoom = this.options.crs ? zoom - this._map.options.customCRS[this.options.crs].startZoom : zoom,
-      translate = origin.multiplyBy(scale)
-        .subtract(this._map._getNewPixelOrigin(center, zoom, this.options.crs)).round()
+    const scale = this._map.getZoomScale(zoom, level.zoom, this.options.crs)
+    const origin = this.options.crs ? level[this.options.crs] : level.origin
+    const realZoom = this.options.crs
+      ? zoom - this._map.options.customCRS[this.options.crs].startZoom
+      : zoom
+    const translate = origin
+      .multiplyBy(scale)
+      .subtract(this._map._getNewPixelOrigin(center, realZoom, this.options.crs))
+      .round()
     if (Browser.any3d) {
       DomUtil.setTransform(level.el, translate, scale)
     } else {
